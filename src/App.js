6664daab377632f98468d8ffe37c7163a6f5a0c8
Londolani <Londolani0700@gmail.com>
2021-08-09 00:20:20 +0200

    added relative buttons

--- src/App.js
+++ src/App.js
@@ -13,16 +13,26 @@ function App() {
         <div className={classes.MainContainer}>
           <div className={classes.ProductPreview}>
             <img src="https://imgur.com/iOeUBV7.png" alt="Product Preview" />
+            <div className={classes.FeatureData}>
+              <p>{`${new Date().getHours()}:${new Date().getMinutes()}`}</p>
+            </div>
           </div>
           <div className={classes.ProductData}>
             <h1 className={classes.ProductTitle} >{ProductData.title} </h1>
             <p className={classes.ProductDescription}>{ProductData.description}</p>
             <h3 className={classes.SectionHeading}>Select Color</h3>
             <div >
-              <img className={classes.ProductImage} src="https://imgur.com/iOeUBV7.png" alt="Black colored Watch" />
+              <img className={[classes.ProductImage, classes.SelectedProductImage].join(' ')} src="https://imgur.com/iOeUBV7.png" alt="Black colored Watch" />
               <img className={classes.ProductImage} src="https://imgur.com/PTgQlim.png" alt="Red colored Watch" />
               <img className={classes.ProductImage} src="https://imgur.com/Mplj1YR.png" alt="Blue colored Watch" />  
               <img className={classes.ProductImage} src="https://imgur.com/xSIK4M8.png" alt="Purple colored Watch" /> 
+              <h3 className={classes.SectionHeading}>Features</h3>
+              <div>
+                <button className={[classes.FeatureItem, classes.SelectedFeatureItem].join(' ')}>Time</button>
+                <button className={classes.FeatureItem}>Heart Rate</button>
+                <br />
+                <button className={classes.PrimaryButton}>Buy Now</button>
+              </div>
             </div>
           </div>
         </div>
