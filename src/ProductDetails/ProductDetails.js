5dc17255d5f702782d81b187877149607778f5e4
Londolani <Londolani0700@gmail.com>
2021-08-20 23:16:23 +0200

    fixed function and finished tutorial

--- src/ProductDetails/ProductDetails.js
+++ src/ProductDetails/ProductDetails.js
@@ -1,16 +1,31 @@
-import React from 'react';
+ import React from 'react';
 import classes from './ProductDetails.module.css';
 
-const ProductDetails = (props) =>{
-    const colorOptions = props.data.colorOptions.map((item, pos) =>{
-        const classArr = [classes.ProductImage]
-        if(pos == props.currentPreviewImagePos){
+const ProductDetails = (props) => {
+    const colorOptions = props.data.colorOptions.map((item, pos) => {
+        const classArr = [classes.ProductImage];
+        if(pos === props.currentPreviewImagePos){
             classArr.push(classes.SelectedProductImage);
         }
         return(
-            <img key={pos} className={classArr.join(' ')} src={item.imageUrl} alt={item.styleName} onClick={() => props.onColorOptionsClick(pos)} /> 
+            <img key={pos} className={classArr.join(' ')} src={item.imageUrl} alt={item.styleName}
+             onClick={() => props.onColorOptionClick(pos)} /> 
         );
     })
+
+    const featureList = props.data.featureList.map((item, pos) =>{
+        const classArr = [classes.FeatureItem];
+        if(pos === props.currentSelectedFeature){
+            classArr.push(classes.SelectedFeatureItem);
+        }
+        
+        return(
+            <button onClick={() => props.onFeatureItemClick(pos)} key={pos} className=
+            {classArr.join(' ')}>{item}</button>
+        );
+    })
+
+
     return(
         <div className={classes.ProductData}>
             <h1 className={classes.ProductTitle} >{props.data.title} </h1>
@@ -18,14 +33,19 @@ const ProductDetails = (props) =>{
             <h3 className={classes.SectionHeading}>Select Color</h3>
             <div >
               {colorOptions}
-              
+
               <h3 className={classes.SectionHeading}>Features</h3>
+
+              {featureList}
+              
+              {/*
+              <h3 className={classes.SectionHeading}>Features22</h3>
               <div>
                 <button className={[classes.FeatureItem, classes.SelectedFeatureItem].join(' ')}>Time</button>
                 <button className={classes.FeatureItem}>Heart Rate</button>
                 <br />
                 <button className={classes.PrimaryButton}>Buy Now</button>
-              </div>
+              </div>*/}
             </div>
         </div>
 
